f600d25399e18f9307bf00b6d09a5c34e4b4dc2d
Spydr06 <spydr06@web.de>
2023-12-01 22:21:52 +0100

    improve part2 further

--- 2023/day_01/Part2.hs
+++ 2023/day_01/Part2.hs
@@ -5,11 +5,11 @@ import Data.Text (pack, unpack, replace)
 import Data.Char
 
 main :: IO ()
-main = readFile "input.txt" >>= print . sum . map (read . ends . filter isDigit . translateDigit [
+main = readFile "input.txt" >>= print . sum . map (read . ends . filter isDigit) . lines . translateDigit [
            ("one", "o1e"), ("two", "t2o"), ("three", "t3e"), ("four", "f4r"), ("five", "f5e"), 
            ("six", "s6x"), ("seven", "s7n"), ("eight", "e8t"), ("nine", "n9e")
-        ]) . lines
+        ]
     where ends xs = [head xs, last xs]
-          translateDigit [] x = x
-          translateDigit xs x = translateDigit (tail xs) $ unpack $ uncurry replace (head xs) $ pack x
+          translateDigit [] = id
+          translateDigit xs = translateDigit (tail xs) . unpack . uncurry replace (head xs) . pack
 
